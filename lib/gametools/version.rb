49cba17577208ca279862cfd091cbd379bf0e4ec
Noel Cower <ncower@gmail.com>
2013-08-12 13:54:40 -0700

    Version 0.0.1pre3.

--- lib/gametools/version.rb
+++ lib/gametools/version.rb
@@ -1,6 +1,6 @@
 module GT
-  GAMETOOLS_VERSION       = '0.0.1pre2'
-  GAMETOOLS_DATE          = '2013-07-28'
+  GAMETOOLS_VERSION       = '0.0.1pre3'
+  GAMETOOLS_DATE          = '2013-08-12'
   GAMETOOLS_LICENSE_BRIEF = 'Simplified BSD'
   GAMETOOLS_PATH          = File.expand_path("../../../", __FILE__)
   GAMETOOLS_LICENSE_FULL  = File.open("#{GAMETOOLS_PATH}/COPYING") { |io| io.read }
